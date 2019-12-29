3c85ae0b7d8b1d9da7e659530f52e85046876e57
Sean Klein <smklein@google.com>
2019-12-29 11:54:53 -0800

    [test] Unit test pseudo-code

--- lib/diff-test.cc
+++ lib/diff-test.cc
@@ -5,10 +5,30 @@ TEST(DiffTest, OnePlusOne) {
   EXPECT_EQ(2, 1 + 1);
 }
 
-TEST(DiffTest, NullDiff) {
-  auto result = Diff("", "");
-  for (auto e : result) {
-    std::cout << std::get<0>(e) << ", " << std::get<1>(e) << std::endl;
-  }
-  EXPECT_TRUE(result.empty());
+TEST(DiffTest, NullComparison) {
+  EXPECT_EQ({}, Diff("", ""));
+}
+
+TEST(DiffTest, IdenticalStrings) {
+  EXPECT_EQ({ Same("ABC") }, Diff("ABC", "ABC"));
+}
+
+TEST(DiffTest, DistinctStrings) {
+  EXPECT_EQ({ Remove("ABC"), Add("DEF") }, Diff("ABC", "DEF"));
+}
+
+TEST(DiffTest, PrefixInsertion) {
+  EXPECT_EQ({ Add("ABC"), Same("DEF") }, Diff("DEF", "ABCDEF"));
+}
+
+TEST(DiffTest, SuffixInsertion) {
+  EXPECT_EQ({ Same("ABC"), Add("DEF") }, Diff("ABC", "ABCDEF"));
+}
+
+TEST(DiffTest, PrefixDeletion) {
+  EXPECT_EQ({ Remove("ABC"), Same("DEF") }, Diff("ABCDEF", "DEF"));
+}
+
+TEST(DiffTest, SuffixDeletion) {
+  EXPECT_EQ({ Same("ABC"), Remove("DEF") }, Diff("ABCDEF", "ABC"));
 }
